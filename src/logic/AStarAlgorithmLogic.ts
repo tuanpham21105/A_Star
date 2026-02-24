79f17577da684b5b1fb786227fd1dc4e18ec0989
tuanpham21105 <tuan.phamminh9936@gmail.com>
2026-02-24 08:05:14 +0700

    fix: Step 0 text wrong order;

--- src/logic/AStarAlgorithmLogic.ts
+++ src/logic/AStarAlgorithmLogic.ts
@@ -86,7 +86,7 @@ function solve() {
     const marker = new Map<string, string>()
 
     pq.push({state: startState, priority: 0})
-    stepsText += steps + "\t|" + "\t|" + "\t|" + "\t|" + "\t|" + "\t|" + "\t|" + "\t|" + queueToString(pq) + "\n"
+    stepsText += steps + "\t|" + "\t|" + "\t|" + "\t|" + "\t|" + "\t|" + "\t|" + queueToString(pq) + "\t|" + "\n"
 
     while (!pq.isEmpty()) {
         steps += 1
